4774b2696bf437649c67a73764464eae7efe77b0
Akash Srivastava <akashom53@gmail.com>
2025-06-09 15:15:50 +0530

    Increased timeout

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,8 +4,13 @@ import { ChatService } from './chat.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.register({
+      timeout: 60000, // 60 seconds timeout
+      maxRedirects: 2,
+    })
+  ],
   controllers: [ChatController],
   providers: [ChatService],
 })
-export class ChatModule {}
+export class ChatModule { }
